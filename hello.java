0776f142fd6f426e5ab9707360b462a2cd45c67c
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-03 12:47:43 +0530

    printed my introduction

--- hello.java
+++ hello.java
@@ -4,7 +4,7 @@ class hello
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Name");
+        System.out.println("Enter Name"); 
         String Name=sc.nextLine();
         System.out.println("Hello," + Name);
     }
---
+++ intro.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class intro
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Hello , my name is Anant Tiwari.");
+        System.out.println("I study in Class 10th of St. Peter's School.");
+    }
+}
