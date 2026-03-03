f4731b045d52ca8675f9fcda225f0837c5a5a696
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-03 12:55:22 +0530

    updated

---
+++ sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 1st number");
+        double a=sc.nextDouble();
+        System.out.println("Enter 2nd number");
+        double b=sc.nextDouble();
+
+        double S=a+b;
+        System.out.println("THE SUM IS:"+ S);
+    }
+}
