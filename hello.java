91ae05c35ad497f769058b6a12ddba236b9f4970
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-03 12:36:33 +0530

    printed hello world, hello , name

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class hello
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Name");
+        String Name=sc.nextLine();
+        System.out.println("Hello," + Name);
+    }
+}
