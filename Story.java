7fd4d4e57936c631678e74b51c8e8e800926b988
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-09 23:44:04 +0530

    updated

---
+++ Story.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Story
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Place, Animal and the color");
+        String P=sc.nextLine();
+        String A=sc.nextLine();
+        String C=sc.nextLine();
+
+        System.out.println("The "+C +A );
+    }
+}
