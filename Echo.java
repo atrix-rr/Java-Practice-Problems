e440d7664d836b76024e2c5b70e89ef49082232d
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-09 23:33:58 +0530

    updated

---
+++ Echo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Echo
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Word");
+        String W=sc.nextLine();
+
+        System.out.println("The word is:"+W);
+        System.out.println("The word is:"+W);
+        System.out.println("The word is:"+W);
+    }
+}
