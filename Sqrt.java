cad07725a1f0ee41ac206d1483321cdb609051a2
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-12 19:30:32 +0530

    updated

---
+++ Sqrt.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Sqrt
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the number");
+        int r=sc.nextInt();
+
+        double S=Math.sqrt(r);
+
+        System.out.println("The Square Root is:"+S);
+
+    }
+}
