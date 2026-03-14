e6481779fe1b0a4e058fc299d08ae49b56977a68
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-14 23:19:24 +0530

    updated

---
+++ Remove.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Remove
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int num=sc.nextInt();
+
+        int result=num/10;
+
+        System.out.println(result);
+    }
+}
