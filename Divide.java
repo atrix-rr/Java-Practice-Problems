53e4bed7d1fbcbeb3b1fdee970bcf4b8365045b8
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-20 21:59:31 +0530

    updated

---
+++ Divide.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Divide
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter 2 numbers");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        double D=a/b;
+
+        System.out.println("The Result is:"+D);
+    }
+}
