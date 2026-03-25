3d8698adf3e4191b928ca5042cfd8ff0aedc7f58
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-25 21:41:23 +0530

    updated

---
+++ Divisibility.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Divisibility
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter a Number");
+        int Num=sc.nextInt();
+
+        if(Num%5==0 && Num%11==0)
+        {
+            System.out.println("It is Divisible");
+        }
+        else 
+        {
+            System.out.println("Not Divisible");
+        }
+    }
+}
