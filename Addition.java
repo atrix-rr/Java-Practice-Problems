f1b61ef8569552a5d03a12ce16e964c8234c7c29
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-27 18:19:49 +0530

    updated

---
+++ Addition.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Addition
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter 2 Numbers");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        int S=a+b;
+
+        System.out.println("The Sum is:"+S);
+        
+    }
+}
