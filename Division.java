fc4bc6cd56fd2bf30904a97ffcb9c293941b1bbf
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-28 23:29:01 +0530

    updated

---
+++ Division.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Division
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter 2 Number");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        double D=a/b;
+
+        System.out.println("The Result is:"+D);
+        
+    }
+}
