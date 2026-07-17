1b2aa6ef97f5ab15db8d64fecec89820d071548f
Anant Tiwari <ananttiwari231@gmail.com>
2026-07-17 23:02:33 +0530

    updtaed

---
+++ MenuDriven.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class MenuDriven {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("-----MENU-----");
+        System.out.println("1) Area of Circle");
+        System.out.println("2) Area of Rectangle");
+        System.out.println("3) Area of Square");
+        System.out.println("Enter your Choice");
+        int ch = sc.nextInt();
+
+        switch (ch) {
+            case 1:
+                System.out.println("Enter Radius");
+                double R = sc.nextDouble();
+                double A = 3.14 * R * R;
+                System.out.println("Area is:" + A);
+                break
+
+            case 2:
+                System.out.println("Enter Length and Breadth");
+                double L = sc.nextDouble();
+                double B = sc.nextDouble();
+                double Ar = L * B;
+                System.out.println("Area is:" + Ar);
+
+            case 3:
+                System.out.println("Enter Side");
+                double S = sc.nextDouble();
+                double a = ;
+                System.out.println("Area is:" + a);
+
+            default:
+                System.out.println("Invalis choice");
+                ;
+
+        }
+    }
+}
