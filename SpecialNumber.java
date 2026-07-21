a4eaf8af7fa41db3ca87b7724428bb333663adb9
Anant Tiwari <ananttiwari231@gmail.com>
2026-07-21 13:43:57 +0530

    updated

---
+++ SpecialNumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class SpecialNumber {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int num = sc.nextInt();
+
+        int f,n=0
+
+    }
+}
